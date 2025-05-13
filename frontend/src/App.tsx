ccbaae8165565e4e9783a42158362b67925fe275
Mohsen Sarrafan <mohsensarrafanc@ucla.edu>
2025-05-13 13:31:54 -0700

    help page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,8 @@ import SignUpPage from './pages/loginPages/signup';
 import ForgotPasswordPage from './pages/loginPages/forgotPassword.tsx';
 import ForgotPasswordSuccessPage from './pages/loginPages/forgotPasswordSuccess.tsx';
 import HomePage from './pages/home/home';
-import NavBar from './pages/NavBar.tsx';
+import NavBar from './pages/General/NavBar.tsx';
+import Help from './pages/General/Help.tsx';
 import {AuthContextWrapper} from './firebase/firebaseAuth.tsx';
 
 const App = () => {
@@ -19,6 +20,7 @@ const App = () => {
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/resetPassword" element={<ForgotPasswordPage />} />
           <Route path="/resetPasswordSuccess" element={<ForgotPasswordSuccessPage />}/>
+          <Route path="/help" element={<Help />} />
         </Routes>
       </Router>
     </AuthContextWrapper>
--- frontend/src/pages/General/NavBar.tsx
+++ frontend/src/pages/General/NavBar.tsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
-import { AuthContext } from '../firebase/firebaseAuth';
+import { AuthContext } from '../../firebase/firebaseAuth';
 
 export default function NavBar() {
   const { user } = useContext<any>(AuthContext);
   const profilePicUrl = user ? user.photoURL : "";
 
   return (
-    <nav className="bg-gradient-to-r from-[#9B5DE5] via-[#00BBF9] to-[#F15BB5] px-6 py-3 flex justify-between items-center shadow-lg">
+    <nav className="sticky top-0 left-0 z-50 bg-gradient-to-r from-[#9B5DE5] via-[#00BBF9] to-[#F15BB5] px-6 py-3 flex justify-between items-center shadow-lg">
       
       <div className="flex items-center">
         {profilePicUrl ? (
@@ -23,8 +23,8 @@ export default function NavBar() {
 
       <div className="flex space-x-6 items-center text-white font-sans font-semibold text-sm">
         <Link to="/help" className="hover:text-[#FEE440] transition">Help</Link>
-        <Link to="/join" className="hover:text-[#FEE440] transition">Join Game</Link>
-        <Link to="/create" className="hover:text-[#FEE440] transition">Create Game</Link>
+        <Link to="/settings" className="hover:text-[#FEE440] transition">Settings</Link>
+        <Link to="/Logout" className="hover:text-[#FEE440] transition">Log Out</Link>
       </div>
     </nav>
   );
