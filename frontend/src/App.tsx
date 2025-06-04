6ba5f53e3ef8782df97547abdef66437c521dce6
Jainxu <mhtran106@g.ucla.edu>
2025-06-04 13:55:00 -0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import ForgotPasswordPage from './pages/loginPages/forgotPassword.tsx';
 import ForgotPasswordSuccessPage from './pages/loginPages/forgotPasswordSuccess.tsx';
 import ProfilePage from './pages/user/MyProfile.tsx';
 import EditProfile from './pages/user/EditProfile.tsx';
-import VerifyEmail from './pages/user/VerifyEmail.tsx';
+import VerifyEmail from './pages/loginPages/VerifyEmail.tsx';
 import HomePage from './pages/home/home';
 import Help from './pages/General/Help.tsx';
 import FindGame from './pages/General/FindGame.tsx';
@@ -32,23 +32,14 @@ const App = () => {
           <Route path="/help" element={<Help />} />
           <Route path="/findGame" element={<FindGame />} />
           <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/profile/edit" element={<EditProfile />} /> 
-          <Route path="/profile/verifyemail" element={<VerifyEmail />} /> 
+          <Route path="/profile/verify" element={<VerifyEmail />} />
+          <Route path="/profile/edit" element={<EditProfile />} />
           {/* <Route path="/game/lobby" element={<GameLobby />} /> */}          <Route path="/game/lobby/:roomId" element={<GameLobby />} />
           <Route path="/game/play/:roomId" element={<GamePlay />} />
           <Route path="/game/prompts/:roomId" element={<PromptPage />} />
           <Route path="/game/caption/:roomId" element={<CaptionPage />} />
           <Route path="/game/voting/:roomId" element={<VotingPage />} />
           <Route path="/game/results/:roomId" element={<ResultsPage />} />
-          <Route path="/profile/edit" element={<EditProfile />} /> 
-          <Route path="/profile/verifyemail" element={<VerifyEmail />} /> 
-          <Route path="/game/lobby" element={<GameLobby />} />
-          <Route path="/game/lobby/:roomId" element={<GameLobby />} />
-          <Route path="/game/play" element={<GamePlay />} />
-          <Route path="/game/prompts" element={<PromptPage />} />
-          <Route path="/game/caption" element={<CaptionPage />} />
-          <Route path="/game/voting" element={<VotingPage />} />
-          <Route path="/game/results" element={<ResultsPage />} />
           <Route path="/test" element={<DbTester />} />
           <Route path="*" element={<NotFound />} />        </Routes>
       </Router>
