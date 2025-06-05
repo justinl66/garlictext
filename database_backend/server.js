f0ce59e953e19ba1192e0a30f6c242dd3c800e96
justinl66 <justinfinity7@gmail.com>
2025-06-04 22:54:19 -0700

    increased drawing image size limit

--- database_backend/server.js
+++ database_backend/server.js
@@ -10,8 +10,8 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to GarlicText Database Backend' });
